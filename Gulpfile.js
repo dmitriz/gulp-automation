14dc560c0a49920699adecefc7e65d2d3cac2167
Dmitri Zaitsev <dmitri14@gmail.com>
2015-04-13 08:14:53 +0100

    add config and uglify

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,14 +2,25 @@
 
 var gulp = require('gulp');
 
+// import local config
+var config = require('./Gulp.conf.js');
+
 // lazy load plugins - $$.clean instead of requiring it
 // don't forget to npm install the plugins!
 var $$ = require('gulp-load-plugins')();
 
+// The default task - called when you run `gulp` from cli
 gulp.task('default', ['build'], function () {
-	console.log("Gulp runs task: default");
 });
 
-gulp.task('build', function () {
-	console.log("Gulp runs task: build");
+gulp.task('build', ['js'], function () {
+});
+
+// compiling js files
+gulp.task('js', function () {
+  //var filter = $$.filter(config.globs.js);
+
+  return gulp.src(config.paths.js)
+    .pipe($$.uglify())
+    .pipe(gulp.dest(config.dirs.min));
 });
---
+++ gulp.conf.js
@@ -0,0 +1,13 @@
+// gulp.conf.js -- Gulp configuration
+module.exports = {
+	paths: {
+
+		// globs for JavaScript files
+		js: "*.js"
+	},
+	dirs: {
+
+		// directory for minimized files
+		min: "min"
+	}
+}
