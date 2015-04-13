7e3642d5b5b69c9c467bf5e5f86609f296b9566e
Dmitri Zaitsev <dmitri14@gmail.com>
2015-04-13 07:58:29 +0100

    add gulp plugins

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,5 +2,9 @@
 
 var gulp = require('gulp');
 
+// lazy load plugins - $$.clean instead of requiring it
+// don't forget to npm install the plugins!
+var $$ = require('gulp-load-plugins')();
+
 gulp.task('default', function () {
 });
