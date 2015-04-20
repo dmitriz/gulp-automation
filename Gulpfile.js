6ca07c2ad7f5be10608dcb8a9e718c3c879adb28
Dmitri Zaitsev <dmitri14@gmail.com>
2015-04-20 07:36:53 +0100

    update comments and cleanup

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/*
+ * --->  Loading External Node.js Modules  <------
+ */
 var gulp = require('gulp');
 var connect = require('connect');
 var connectLivereload = require('connect-livereload');
@@ -10,21 +13,23 @@ var gulpLivereload = require('gulp-livereload');
  * ---------->  Main Config  <-------------
  */
 var config = {
+
+  // this is your local directory to become served as root, 
+  // e.g. `localhost:8080` should point to show `index.html` in that directory
 	rootDir: __dirname,
+
+  // any port to use for your local server
 	servingPort: 8080,
 
 	// the files you want to watch for changes for live reload
-	filesToWatch: ['*.{html,css,js}', '!Gulpfile.js', '!karma.conf.js', '!protractor.conf.js']
+  // replace by any glob pattern matching your files
+	filesToWatch: ['*.{html,css,js}', '!Gulpfile.js']
 }
-
-// To keep Travis CI happy
-gulp.task('test', function () {
-  console.log("Travis is happy with you!");
-});
 
 // The default task - called when you run `gulp` from CLI
 gulp.task('default', ['watch', 'serve']);
 
+// `gulp watch` task watching for file changes
 gulp.task('watch', ['connect'], function () {
   gulpLivereload.listen();
   gulp.watch(config.filesToWatch, function(file) {
@@ -33,10 +38,12 @@ gulp.task('watch', ['connect'], function () {
   });
 });
 
+// `gulp serve` task loading the URL in your browser 
 gulp.task('serve', ['connect'], function () {
   return opn('http://localhost:' + config.servingPort);
 });
 
+// `gulp connect` task starting your server
 gulp.task('connect', function(){
   return connect()
     .use(connectLivereload())
